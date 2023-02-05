a7c5525a55b1636fe9b1e9040348db0089fbc9fa
rinchoku <gazar.24865@gmail.com>
2023-02-05 09:23:41 +0900

    chore: test path parameters

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import os 
 
+from markupsafe import escape
 from flask import Flask
 
 
@@ -9,6 +10,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    return 'Hello World!!!!!!'
+
+@app.route('/demo/variables/all/<user_id>')
+def demoAll(user_id):
+    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+
+@app.route('/demo/variables/string/<string:user_id>')
+def demoString(user_id):
+    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+
+@app.route('/demo/variables/int/<int:user_id>')
+def demoInt(user_id):
+    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+
+@app.route('/demo/variables/path/<path:user_id>')
+def demoPath(user_id):
+    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+
+@app.route('/demo/variables/uuid/<uuid:user_id>')
+def demoUUID(user_id):
+    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
