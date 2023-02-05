f8942e60d5142c9c3ff0663f6e17b1d78aaa1641
rinchoku <gazar.24865@gmail.com>
2023-02-05 09:26:00 +0900

    chore: rename h1 title to identify

--- src/app.py
+++ src/app.py
@@ -13,23 +13,23 @@ def index():
 
 @app.route('/demo/variables/all/<user_id>')
 def demoAll(user_id):
-    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+    return f'<h1>Demo ALL</h1><p>{escape(user_id)}</p>'
 
 @app.route('/demo/variables/string/<string:user_id>')
 def demoString(user_id):
-    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+    return f'<h1>Demo String</h1><p>{escape(user_id)}</p>'
 
 @app.route('/demo/variables/int/<int:user_id>')
 def demoInt(user_id):
-    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+    return f'<h1>Demo Int</h1><p>{escape(user_id)}</p>'
 
 @app.route('/demo/variables/path/<path:user_id>')
 def demoPath(user_id):
-    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+    return f'<h1>Demo Path</h1><p>{escape(user_id)}</p>'
 
 @app.route('/demo/variables/uuid/<uuid:user_id>')
 def demoUUID(user_id):
-    return f'<h1>Demo</h1><p>{escape(user_id)}</p>'
+    return f'<h1>Demo UUID</h1><p>{escape(user_id)}</p>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
